a51e2f1c54b772de447d81d2702d575c5bee5a83
NamoeL19 <manumachadog@gmail.com>
2026-06-08 16:02:20 -0300

    Começo do drag and drop

---
+++ Scripts/minigame/slot.gd
@@ -0,0 +1,9 @@
+extends TextureRect
+
+func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
+	return true
+	return false
+	
+func _drop_data(at_position: Vector2, data: Variant) -> void:
+	data[0].get_parent().remove_child(data[0])
+	add_child(data[0])
